0dde459f8235ac22e29754912aa62ac78b2f1b1c
Miika Tuominen <miika.km.tuominen@gmail.com>
2024-07-03 21:44:31 +0300

    add delay to screenshot script

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -188,6 +188,8 @@
       go
       cargo
       gnome.gnome-software
+      grim
+      slurp
     ]);
   };
 
--- .config/nix/fetches.nix
+++ .config/nix/fetches.nix
@@ -1,31 +1,32 @@
-{ self,  unstable, ... }: {
-  (with unstable; [
-      fetch-scm
-      fetchutils
-      afetch
-      hayabusa
-      owofetch
-      maxfetch
-      bunnyfetch
-      yafetch
-      leaf
-      nitch
-      screenfetch
-      disfetch
-      bfetch
-      nerdfetch
-      fet-sh
-      profetch
-      macchina
-      uwufetch
-      starfetch
-      ramfetch
-      pridefetch
-      pfetch
-      pfetch-rs
-      neofetch
-      hyfetch
-      freshfetch
-      fastfetch
-    ]);
-  }
+with (import <nixpkgs> { });
+mkShell {
+  shellHook = "zsh";
+  buildInputs = [
+    fetch-scm
+    afetch
+    # hayabusa # proprietary
+    owofetch
+    maxfetch
+    bunnyfetch
+    yafetch
+    leaf
+    nitch
+    screenfetch
+    disfetch
+    bfetch
+    nerdfetch
+    fet-sh
+    profetch
+    macchina
+    # uwufetch # segfaults
+    starfetch
+    ramfetch
+    pridefetch
+    pfetch
+    pfetch-rs
+    neofetch
+    hyfetch
+    freshfetch
+    fastfetch
+  ];
+}
