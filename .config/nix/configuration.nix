10f7810720a3037d31264053531a141702d9c0ae
Miika Tuominen <miika.km.tuominen@gmail.com>
2024-07-04 17:11:47 +0300

    power mgmt

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -48,9 +48,31 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+    };
+  };
+
+  powerManagement.enable = true;
+  powerManagement.powertop.enable = true;
 
   environment.localBinInPath = true;
-  environment.systemPackages = with pkgs; [ wget git gptfdisk wireguard-tools ];
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    gptfdisk
+    wireguard-tools
+    powertop
+  ];
 
   security.pam.services.passwd.fprintAuth =
     false; # we want passsword to be primary
