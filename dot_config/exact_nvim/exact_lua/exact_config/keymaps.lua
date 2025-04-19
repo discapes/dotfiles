9baa1b18ce969541bac3048b681cac5447b80711
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-04-19 18:44:12 +0300

    nvim - add keymaps

--- dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -10,4 +10,35 @@ map({ "n", "i", "v" }, "<Up>", "<Nop>")
 map({ "n", "i", "v" }, "<Down>", "<Nop>")
 map({ "n", "i", "v" }, "<PageUp>", "<Nop>")
 map({ "n", "i", "v" }, "<PageDown>", "<Nop>")
+--
 map("n", "<leader>L", "<cmd>LazyExtras<cr>", { desc = "LazyExtras" })
+map("n", "<leader>cL", function()
+  local filetype = vim.bo.filetype
+  local linters = require("lint").linters_by_ft[filetype]
+
+  if linters then
+    print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+  else
+    print("No linters configured for filetype: " .. filetype)
+  end
+end, { desc = "Show active linters" })
+
+-- surround
+map("x", "gsm", ":<C-U>lua MiniSurround.add('visual')<CR>?```<CR>```<CR>gv<gv")
+-- map("x", "gsm", "gsa?```<CR>```<CR>gv<", { remap = true }) can't use remap here since somehow autopairs? makes it ```` instead of ```
+-- another thing to note is that not using remap means you need to gv after <
+map("x", "gsc", "<<<gsmo<Esc>kAsh<Esc>", { remap = true })
+map("n", "gsc", "Vgsc", { remap = true })
+
+-- map("n", "<Esc>", "<C-h>", { remap = true })
+-- map("n", "<Esc>", function()
+--   for i = 1, 3 do
+--     vim.cmd.wincmd("w")
+--     if vim.bo.filetype == "snacks_layout_box" then
+--       break
+--     end
+--   end
+-- end)
+
+-- map("n", "<leader>ue", function toggletransparency()
+--   -- if colorscheme startswith tokyonight, theyn w
