ba0b6ed0257a9d7a3ba9ffa348af43211d58e482
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-04-19 18:31:57 +0300

    update mini.surround conf

---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/override_surround.lua
@@ -0,0 +1,9 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    -- so we can for example surround code blocks with ```
+    -- instead of surrounding each line of the selection
+    respect_selection_type = true,
+  },
+  event = "VeryLazy", -- for keybind to work
+}
