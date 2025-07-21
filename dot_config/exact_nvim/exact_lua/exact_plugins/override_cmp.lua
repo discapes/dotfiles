2d7f0f483b10179ed2ec81edf694eda442da9701
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-07-21 16:35:47 +0300

    fix issue with blink.cmp

--- dot_config/exact_nvim/exact_lua/exact_plugins/override_cmp.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/override_cmp.lua
@@ -4,6 +4,12 @@ return {
   opts = {
     keymap = {
       preset = "super-tab",
+      -- https://github.com/LazyVim/LazyVim/issues/6185
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
     },
   },
 }
