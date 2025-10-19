50e97cdc81c5b52b6ad552a9252cd87fa839d0cd
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-10-19 19:53:33 +0300

    nvim: use new repo address for mason

--- dot_config/exact_nvim/exact_lua/exact_plugins/override_mason.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/override_mason.lua
@@ -1,8 +1,8 @@
-os_result = vim.fn.system({"sh", "-c", "cat /etc/*-release | rg '^NAME=(.*)' -or '$1' | tr -d '\n'"})
+os_result = vim.fn.system({ "sh", "-c", "cat /etc/*-release | rg '^NAME=(.*)' -or '$1' | tr -d '\n'" })
 is_nixos = string.lower(os_result) == "nixos"
-print("nixos: " ..  tostring(is_nixos))
+--print("nixos: " .. tostring(is_nixos))
 
 return {
-    "williamboman/mason.nvim",
-    enabled = not is_nixos
+  "mason-org/mason.nvim",
+  enabled = not is_nixos,
 }
