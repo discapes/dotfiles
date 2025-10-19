67a1bf05fd04860e9476e52a22f0896acfe086be
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-10-19 19:52:52 +0300

    nvim: override clangd args to use g++

--- dot_config/exact_nvim/exact_lua/exact_plugins/override_lspconfig.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/override_lspconfig.lua
@@ -3,6 +3,23 @@ return {
   ---@class PluginLspOpts
   opts = {
     servers = {
+      clangd = {
+        -- https://discourse.nixos.org/t/clang-clang-and-clangd-cant-find-headers-even-with-compile-commands-json/54657
+        mason = false,
+        -- we make clangd use gcc since clang on macOS (both nix and os) don't use libstdc++
+        -- which contains bits/stdc++.h useful for competitive programming
+        -- note that we also need to set g++ ~/.clangd. either of these changes alone will do nothing.
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+          vim.fn.expand("--query-driver=$HOME/.nix-profile/bin/g++"),
+        },
+      },
       pyright = {
         settings = {
           python = {
