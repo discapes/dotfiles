3569cb1b3b4b6915a69627366709a5c0eda27ac3
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-11-07 21:05:10 +0200

    add markview, disabled

---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/new_markview.lua
@@ -0,0 +1,13 @@
+-- render exponents as superscript in typst
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+  enabled = false,
+  opts = {
+    typst = {
+      code_blocks = {
+        enable = false,
+      },
+    },
+  },
+}
